e2f24bd3b8041fb5eee7632230712fac9d79acb5
daniel-training <danvazmem.training@gmail.com>
2023-11-03 06:23:41 +0100

    feat: implement WebSocket Controller and Configuration for base Chat functionality
    - Add WebSocketConfig Class
    - Add RoomWebSocketController Class
    
    Refs: #sprint-1

---
+++ src/main/java/com/example/chat/configuration/WebSocketConfig.java
@@ -0,0 +1,67 @@
+package com.example.chat.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * Configuration class that prepares Spring to enable WebSocket and STOMP messaging. The message handling are backed by
+ * a message broker. The class implements <code>WebSocketMessageBrokerConfigurer</code>. Overrides
+ * <code>registerStompEndpoints()</code> to set the endpoint to Register/Connect to the Chat WebSocket service
+ * (handshake). And Overrides <code>configureMessageBroker</code> to configure the Endpoints to consume the Chat
+ * services.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    /**
+     * Configures the message Broker
+     * <p>
+     * To subscriptions, configures prefixes Endpoints to consume the Chat services. For example, a client app uses
+     * following endpoints:
+     * <p>
+     * Publish/Subscribe (broadcast to multiple consumers)
+     * <p>
+     * <pre>{@code /topic/{room} }</pre>
+     * <p>
+     * Point to Point (unique consumer) TODO: pending to implement.
+     * <p>
+     * <pre>{@code /queue/{user} }</pre>
+     * <p>
+     * To a client send messages to the server, configures the destination endpoint prefix. For example: Send message
+     * <pre>{@code /app/chat/{room} }</pre>
+     *
+     * @param config registry for configuring message broker options.
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    /**
+     * Register and configures STOMP endpoints mappings.
+     * <p>
+     * A client Registers/Connects to the Chat WebSocket service (handshake) using following endpoint:
+     * <pre>{@code
+     * /chat-websocket-service
+     * }</pre>
+     * Examples:
+     * <pre>{@code ws://exampledomain.com/chat-websocket-service }</pre>
+     * <pre>{@code ws://localhost:8080/chat-websocket-service }</pre>
+     * (*) For the handshake the ws protocol is used to request the WebSocket connection.
+     *
+     * @param registry used for registering STOMP over WebSocket endpoints by providing the methods to make the
+     *                 configuration.
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        registry.addEndpoint("/chat-websocket-service");
+    }
+
+
+}
---
+++ src/main/java/com/example/chat/webSocketController/RoomWebSocketController.java
@@ -0,0 +1,44 @@
+package com.example.chat.webSocketController;
+
+import com.example.chat.model.Message;
+import com.example.chat.model.Room;
+import com.example.chat.model.User;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+
+/**
+ * Controller Class to WebSocket message handling.
+ * <p>
+ * Messages are routed to the methods of the controller that maps with <code>@MessageMapping</code> annotation the
+ * endpoint of destination for incoming messages. Then applies any necessary logic for each case and returns the result
+ * message that is broadcast to all subscribers to the endpoint mapped with the <code>@SendTo</code> annotation
+ */
+@Controller
+public class RoomWebSocketController {
+
+    /**
+     * Handles the incoming message sent from the chat room, formats it including the sender and broadcasts the
+     * message to the subscribers of the corresponding chat room.
+     *
+     * @param room    chat room where the message belongs. Is part of the endpoint path.
+     * @param message incoming message from client sender
+     * @return the message to broadcast formatted with including the sender
+     * @throws Exception TODO ...
+     */
+    @MessageMapping("/chat/{room}")
+    @SendTo("/topic/{room}")
+    public Message inboundMessage(@DestinationVariable String room, Message message) throws Exception {
+
+        User user = message.getUser();
+
+        return new Message(String.format("[%s] %s", user.getNickname(), HtmlUtils.htmlEscape(message.getContent())),
+                user,
+                new Room(room, message.getUser()));
+
+    }
+
+}
