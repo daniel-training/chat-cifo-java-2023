16a1db7c03b4a9bacc88a73a3bf96e4445748d71
daniel-training <danvazmem.training@gmail.com>
2023-11-10 12:06:03 +0100

    feat: adds option to websoket configuration to the origins for which cross-origin requests are allowed

--- src/main/java/com/example/chat/configuration/WebSocketConfig.java
+++ src/main/java/com/example/chat/configuration/WebSocketConfig.java
@@ -59,7 +59,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
      */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat-websocket-service");
+        registry.addEndpoint("/chat-websocket-service").setAllowedOriginPatterns("*");
     }
 
 }
